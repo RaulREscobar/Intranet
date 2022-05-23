13fa6c836e8a62475c7a2bd13829a0e4d234e188
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-05-23 10:55:57 -0300

    inputs para cambiar datos

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -2,13 +2,66 @@ import React from 'react'
 import BarNav from '../components/BarNav'
 import User from '../components/User'
 import TitleSection from '../components/TitleSection'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const EditProfile = () => {
   return (
-    <div>
+    <div className=''>
       < TitleSection title="Hola Raul" />
       < User />
-      <BarNav  />
+      <section className='bg-central pt-3' >
+        <Form inline >
+          <FormGroup>
+            <Label
+              for="name"
+              hidden
+            >
+            </Label>
+            <Input
+              id="name"
+              name="name"
+              placeholder="Nombre"
+              type="text"
+            />
+          </FormGroup>
+          {' '}
+          <FormGroup>
+            <Label
+              for="lastName"
+              hidden
+            >
+            </Label>
+            <Input
+              id="lastName"
+              name="lastName"
+              placeholder="Apellido"
+              type="text"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label
+              for="date"
+              hidden
+            >
+            </Label>
+            <Input
+              id="date"
+              name="date"
+              type="date"
+            />
+          </FormGroup>
+          <Button
+            color="light"
+            outline
+            className='d-flex mx-auto'
+          >
+            <Link to="/editProfile">guardar</Link>
+          </Button>
+        </Form>
+      </section>
+
+      <BarNav />
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
-import { 
+import {
   Card,
-  CardText, 
-  CardTitle, 
+  CardText,
+  CardTitle,
   CardBody,
   Col,
   CardImg,
- } from 'reactstrap'
+} from 'reactstrap'
 import FormLogin from '../components/FormLogin'
 
 function Login() {
   return (
     <div className='container login'>
-        <Col sm="4">
-                    <Card color='light' >
-                        <CardBody>
-                            <CardTitle tag="h5">
-                                <div className='logo'>
-                                <CardImg
-                               src="https://nets.com.ar/wp-content/uploads/2022/02/cropped-Logo-empresa-comunicacion-simple-morado-1.png"
-                               />
-                                </div>
-                            </CardTitle>
-                            <CardText>
-                                <FormLogin />
-                            </CardText>
-                        </CardBody>
-                    </Card>
-                </Col>
+      <Col sm="4">
+        <Card color='light' >
+          <CardBody>
+            <CardTitle tag="h5">
+              <div className='logo'>
+                <CardImg
+                  src="https://nets.com.ar/wp-content/uploads/2022/02/cropped-Logo-empresa-comunicacion-simple-morado-1.png"
+                />
+              </div>
+            </CardTitle>
+            <CardText>
+              <FormLogin />
+            </CardText>
+          </CardBody>
+        </Card>
+      </Col>
     </div>
   )
 }
