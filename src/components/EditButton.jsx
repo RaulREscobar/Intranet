703fd7b07d78a54b908872b0915650eb8db4116d
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-05-28 21:37:30 -0300

    se arregla boton de edicion de usuario

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export const EditButton = ({url}) => {
     return (
-    <Link to={{url}} className='icon'>
+    <Link to={url} className='icon'>
             <i class="bi bi-pencil edit"></i>       
     </Link>
   )
