41bf0cda733dfcda0b2baeb2e2842555bd749148
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-06-04 15:19:22 -0300

    se arregla que cierre el modal

--- src/components/DataToEdit.jsx
+++ src/components/DataToEdit.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react'
 import { EditButton } from './EditButton'
-//import Modal from './Modal'
 import './Modal.css'
 
 const DataToEdit = ({ data, isOpen, closeModal, openModal }) => {
-       return (
+    const handleModalContainerClick = (e) => e.stopPropagation();   
+    
+    return (
         <div className='d-flex justify-content-between date' >
             <span>
                 {data}
             </span>
             <span className='position-relative' onClick={openModal}>
                 < EditButton url="" />
+            </span>
                 <article className={`modals ${isOpen && 'is-open'}`} onClick={closeModal}>
-                    <div className="modal-container">
+                    <div className="modal-container" onClick={handleModalContainerClick}>
                         <button className="modal-close" onClick={closeModal}>
                             X
                         </button>
                         <h2>{data}</h2>
                     </div>
                 </article>
-            </span>
+            
         </div>
     )
 }
