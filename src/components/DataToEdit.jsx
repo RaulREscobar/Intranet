c3b40243f8ab73c983d24dcb1337f176248fb487
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-06-04 18:47:27 -0300

    se estiliza inputs y se da funcionalidad al modal

--- src/components/DataToEdit.jsx
+++ src/components/DataToEdit.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Input, FormGroup, Button, Form } from 'reactstrap'
 import { EditButton } from './EditButton'
 import './Modal.css'
 
@@ -19,19 +20,40 @@ const DataToEdit = ({ type, data, isOpen, closeModal, openModal }) => {
             <span className='position-relative' onClick={openModal}>
                 < EditButton url="" />
             </span>
-            <article className={`modals ${isOpen && 'is-open'}`} onClick={closeModal}>
-                <div className="modal-container" onClick={handleModalContainerClick}>
-                    <button className="modal-close" onClick={closeModal}>
-                        X
-                    </button>
-                    <form onSubmit={changeData}>
-                        <input
-                            id='name'
-                            type={type}
-                            placeholder={dato}
-                        />
-                        <input type="submit" value="Guardar" />
-                    </form>
+            <article
+                className={`modals ${isOpen && 'is-open'}`}
+                onClick={closeModal}
+            >
+                <div
+                    className="modal-container"
+                    onClick={handleModalContainerClick}
+                >
+                    <Button
+                        color='darck'
+                        className="modal-close"
+                        onClick={closeModal}
+                    >
+                        <i class="bi bi-x-square-fill"></i>
+                    </Button>
+                    < Form
+                        onSubmit={changeData}
+                        className="mt-4"
+                    >
+                        <FormGroup>
+                            <Input
+                                id='name'
+                                type={type}
+                                placeholder={dato}
+                            />
+                        </FormGroup>
+                        < FormGroup>
+                            <Input
+                                type="submit"
+                                className='bg-dark text-light'
+                                value="Guardar"
+                            />
+                        </FormGroup>
+                    </Form>
                 </div>
             </article>
         </div>
